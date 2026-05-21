e02be21a6bb459bcf12b5079c853757d0764ed1c
danil abramov <d.abramov@allerhand.ru>
2026-05-21 16:52:03 +0300

    Update appValues

---
+++ lib/core/theme/app_values.dart
@@ -0,0 +1,9 @@
+abstract final class AppValues{
+  /// Duration
+  static const Duration routeTransitionDuration = Duration(milliseconds: 400);
+  static const Duration queryTimeoutDuration = Duration(seconds: 15);
+
+  /// Routes
+  static const String wrapperInitPath = '';
+  static const String splashPath = "/";
+}
